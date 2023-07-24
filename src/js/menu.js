c7d7e1c28e8f2591b56048a0ed58a634d8dc8511
ammaralialis <ammaralialiwa@gmail.com>
2023-07-24 15:24:29 -0600

    resolve linter error

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,18 +1,17 @@
 const IconClose = document.getElementById('IconClose');
-const menu_container = document.getElementById('menu_container');
+const menuContainer = document.getElementById('menu_container');
 const openMenuIcon = document.getElementById('openMenuIcon');
-const body = document.getElementById('body');
 const linksMenu = document.querySelectorAll('.linksMenu');
-function openMenu(){
-    menu_container.style.display = 'block';
-    document.body.style.overflow = 'hidden';
+function openMenu() {
+  menuContainer.style.display = 'block';
+  document.body.style.overflow = 'hidden';
 }
-function closeMenu(){
-    menu_container.style.display = 'none';
-    document.body.style.overflow = 'auto';
+function closeMenu() {
+  menuContainer.style.display = 'none';
+  document.body.style.overflow = 'auto';
 }
 openMenuIcon.addEventListener('click', openMenu);
 IconClose.addEventListener('click', closeMenu);
-linksMenu.forEach(link => {
-    link.addEventListener('click', closeMenu);
+linksMenu.forEach((link) => {
+  link.addEventListener('click', closeMenu);
 });
